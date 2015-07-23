3f6a303658c885ddee8183d96ea671946463457c
Brian Flynn <bflynn@eclipseoptions.com>
2015-07-23 09:36:16 +0800

    Overwrite CommonAssemblyInfo.cs with new version

--- src/package/version.ps1
+++ src/package/version.ps1
@@ -5,4 +5,4 @@ param([String] $commonAssemblyInfo, [String]$build)
 $assemblyInfo = Get-Content $commonAssemblyInfo
 $version = '$1.' + $build
 $replace = 'Version("' + $version + '")'
-$assemblyInfo -Replace  'Version\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+)?\"\)', $replace
+$assemblyInfo -Replace  'Version\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+)?\"\)', $replace | Out-File $commonAssemblyInfo
