e83a973fb377ecaa1ce6805e8dbc4a1c1558b8c3
Brian Flynn <bflynn@eclipseoptions.com>
2015-09-10 12:00:02 +0800

    Added Support for Child Views in Presenter, and added ActiveViewer control to display child views in a view

---
+++ src/Controls/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aurora.Controls")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("12e7fe01-c52e-42fd-a288-b0ad1e1c2af1")]
--- src/Framework/Core/ViewPresenter.cs
+++ src/Framework/Core/ViewPresenter.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Aurora.Core.Activities;
+using Microsoft.Practices.Unity;
 
 namespace Aurora.Core
 {
@@ -6,10 +10,32 @@ namespace Aurora.Core
         where TViewModel : IViewModel
         where TActivityInfo : ViewActivityInfo
     {
+        private readonly List<ActiveView> childViews = new List<ActiveView>();
+
         public ViewPresenter(TActivityInfo viewActivityInfo) : base(viewActivityInfo)
         {
         }
 
+        [Dependency]
+        public IViewFactory ViewFactory { get; set; }
+
         public ViewActivityInfo ViewActivityInfo => ActivityInfo;
+
+        protected virtual async Task<ActiveView> AddChildView(Type presenterType, params object[] parameters)
+        {
+            var view = await ViewFactory.CreateActiveViewAsync(presenterType, parameters);
+            this.childViews.Add(view);
+            return view;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            foreach (var activeView in childViews)
+            {
+                activeView.Presenter.Dispose();
+            }
+        }
     }
 }
