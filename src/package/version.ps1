65d1f15f4e8e737c9500dba2ac62c351691b3d06
unknown <bflynn@BRIANFLYNN.hk.eclipseoptions.com>
2015-09-22 12:26:23 +0800

    Updated Version Script

--- src/package/version.ps1
+++ src/package/version.ps1
@@ -1,4 +1,4 @@
-param([String] $commonAssemblyInfo, [String]$build)
+param([String] $commonAssemblyInfo, [String]$build, [String] $env)
 
 "Updating Version Build Number To: " + $build
 
@@ -6,3 +6,8 @@ $assemblyInfo = Get-Content $commonAssemblyInfo
 $version = '$1.' + $build
 $replace = 'Version("' + $version + '")'
 $assemblyInfo -Replace  'Version\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+)?\"\)', $replace | Out-File $commonAssemblyInfo
+
+$assemblyInfo = Get-Content $commonAssemblyInfo
+$informationalVersion = '$1.' + $build + '-' + $env
+$replace = 'AssemblyInformationalVersion("' + $informationalVersion  + '")'
+$assemblyInfo -Replace 'AssemblyInformationalVersion\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+\-DEV)?\"\)', $replace | Out-File $commonAssemblyInfo
