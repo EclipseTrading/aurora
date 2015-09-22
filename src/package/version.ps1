1178993e7934f588b9e0023b03b8e2e06f929a06
unknown <bflynn@BRIANFLYNN.hk.eclipseoptions.com>
2015-09-22 13:30:38 +0800

    Updated the version script to reference the correct version of Aurora packages and use the pre-release version string for pre-release builds

--- src/package/version.ps1
+++ src/package/version.ps1
@@ -2,12 +2,31 @@ param([String] $commonAssemblyInfo, [String]$build, [String] $env)
 
 "Updating Version Build Number To: " + $build
 
+# If Prod Use the Assembly Version, otherwise take the version with the environment / JIRA at the end, i.e. pre-release version string
+$verString = If($env -eq 'PROD') { 'AssemblyVersion' } ELSE { 'AssemblyInformationalVersion' } 
+
+# Update the AssemblyVersion and AssemblyFile Version
 $assemblyInfo = Get-Content $commonAssemblyInfo
 $version = '$1.' + $build
 $replace = 'Version("' + $version + '")'
 $assemblyInfo -Replace  'Version\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+)?\"\)', $replace | Out-File $commonAssemblyInfo
 
+# Update the AssemblyInformationalVersion
 $assemblyInfo = Get-Content $commonAssemblyInfo
 $informationalVersion = '$1.' + $build + '-' + $env
 $replace = 'AssemblyInformationalVersion("' + $informationalVersion  + '")'
 $assemblyInfo -Replace 'AssemblyInformationalVersion\(\"([0-9]+\.[0-9]+\.[0-9]+)(\.[0-9]+\-DEV)?\"\)', $replace | Out-File $commonAssemblyInfo
+
+# Get the version string to tag the nuget package with
+$pattern = '\[assembly\:\s*'+$verString+'\(\"(.*)\"\)'
+$match = Get-Content $commonAssemblyInfo | Select-String -Pattern $pattern | Select-Object -first 1
+$version = $match.Matches[0].Groups[1].Value
+
+# Update all the nuget files to reference the version in this build
+Get-ChildItem . -Filter *.nuspec | `
+Foreach-Object {
+  $content = Get-Content $_.Name 
+  $replace = '<dependency id="$1" version="' + $version + '"/>'
+  $dep = '<dependency\s*id=\"(Aurora\.[a-zA-Z0-9_]*)\"\s*version=\"[0-9\.]*\"\s*/>'  
+  $content -Replace $dep, $replace | Out-File $_.Name
+}
